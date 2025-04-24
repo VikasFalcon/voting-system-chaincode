9951e4db8656bfde73f8ff893aad32669997af28
Vikas S <test@mail.com>
2025-04-24 11:25:11 +0530

    RegisterElection function added

--- main.go
+++ main.go
@@ -19,12 +19,13 @@ type Voter struct {
 }
 
 type Election struct {
-	ElectionID string         `json:"electionID"`
-	Name       string         `json:"Name"`
-	Candidates []string       `json:"Candidates"`
-	Votes      map[string]int `json:"votes"`
-	StartTime  time.Time      `json:"startTime"`
-	EndTime    time.Time      `json:"endTime"`
+	ElectionID string          `json:"electionID"`
+	Name       string          `json:"Name"`
+	Candidates []string        `json:"Candidates"`
+	Votes      map[string]int  `json:"votes"`
+	HasVoted   map[string]bool `json:"hasVoted"`
+	StartTime  time.Time       `json:"startTime"`
+	EndTime    time.Time       `json:"endTime"`
 }
 
 // RegisterVoter - is use to register new unique voter
@@ -50,6 +51,56 @@ func (s *SmartContract) RegisterVoter(ctx contractapi.TransactionContextInterfac
 	}
 
 	return ctx.GetStub().PutState(voterID, voterJSON)
+}
+
+// RegisterElection - is use to register unique election details
+func (s *SmartContract) RegisterElection(ctx contractapi.TransactionContextInterface, electionID, name string, candidates []string, startTime, endTime string) error {
+	isElectionExists, err := s.isStateExists(ctx, electionID)
+	if err != nil {
+		return fmt.Errorf("RegisterElection(): %s", err.Error())
+	}
+
+	if isElectionExists {
+		return fmt.Errorf("RegisterElection(): election is already exists with electionID: %s", electionID)
+	}
+
+	_startTime, err := time.Parse(time.RFC3339, startTime)
+	if err != nil {
+		return fmt.Errorf("RegisterElection(): error while converting startTime in expected format for electionID:: %s, error: %s", electionID, err.Error())
+	}
+
+	_endTime, err := time.Parse(time.RFC3339, endTime)
+	if err != nil {
+		return fmt.Errorf("RegisterElection(): error while converting endTime in expected format for electionID:: %s, error: %s", electionID, err.Error())
+	}
+
+	var votes = make(map[string]int)
+	if len(candidates) < 2 {
+		fmt.Errorf("RegisterElection(): at least 2 candidate require for election")
+	}
+
+	for _, c := range candidates {
+		votes[c] = 0
+	}
+
+	var hasVoted = make(map[string]bool)
+
+	election := Election{
+		ElectionID: electionID,
+		Name:       name,
+		Candidates: candidates,
+		Votes:      votes,
+		HasVoted:   hasVoted,
+		StartTime:  _startTime,
+		EndTime:    _endTime,
+	}
+
+	electionJSON, err := json.Marshal(election)
+	if err != nil {
+		return fmt.Errorf("RegisterElection(): error while marshalling voter with electionID: %s, error:%s", electionID, err.Error())
+	}
+
+	return ctx.GetStub().PutState(electionID, electionJSON)
 }
 
 // isStateExists - is use to check if data already exists in state
