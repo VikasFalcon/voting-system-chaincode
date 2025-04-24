12db8503273975ef351c408e4d3a8966b8b549c2
Vikas S <test@mail.com>
2025-04-24 13:55:35 +0530

    logs corrected for GetFinalElectionResult method

--- main.go
+++ main.go
@@ -187,25 +187,25 @@ func (s *SmartContract) TallyVotes(ctx contractapi.TransactionContextInterface, 
 func (s *SmartContract) GetFinalElectionResult(ctx contractapi.TransactionContextInterface, electionID string) (map[string]int, error) {
 	isElectionExists, err := s.isStateExists(ctx, electionID)
 	if err != nil {
-		return nil, fmt.Errorf("TallyVotes(): %s", err.Error())
+		return nil, fmt.Errorf("GetFinalElectionResult(): %s", err.Error())
 	}
 
 	if !isElectionExists {
-		return nil, fmt.Errorf("TallyVotes(): election does not exists with electionID: %s", electionID)
+		return nil, fmt.Errorf("GetFinalElectionResult(): election does not exists with electionID: %s", electionID)
 	}
 
 	election, err := getState[Election](ctx, electionID)
 	if err != nil {
-		return nil, fmt.Errorf("TallyVotes(): %s", err.Error())
+		return nil, fmt.Errorf("GetFinalElectionResult(): %s", err.Error())
 	}
 
 	currentTime := time.Now()
 	if currentTime.Before(election.StartTime) {
-		return nil, fmt.Errorf("TallyVotes(): election with electionID: %s not started yet", electionID)
+		return nil, fmt.Errorf("GetFinalElectionResult(): election with electionID: %s not started yet", electionID)
 	}
 
 	if currentTime.Before(election.EndTime) {
-		return nil, fmt.Errorf("TallyVotes(): election with electionID: %s not ended yet", electionID)
+		return nil, fmt.Errorf("GetFinalElectionResult(): election with electionID: %s not ended yet", electionID)
 	}
 
 	return election.Votes, nil
